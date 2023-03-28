9d60dd2b2b9ffb69028aec49358b6a9f62027cf8
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2023-03-28 10:52:44 +0200

    Affichage de la modale

--- FrontEnd/js/works.js
+++ FrontEnd/js/works.js
@@ -10,7 +10,7 @@ if (works !== null) {
     getWorks();
     displayWorks(works);
 }
-
+checkConnectStatus();
 
 async function getWorks() {
     let res = await fetch('http://localhost:5678/api/works');
@@ -66,19 +66,62 @@ function sortWorks( btn, category) {
 }
 
 function removeActiveClass() {
-    let filtersBtns = document.querySelectorAll('.filters-btn');
-    for( let i=0; i<filtersBtns.length; i++) {
-        filtersBtns[i].classList.remove('active')
+    document.querySelectorAll('.filters-btn').forEach(btn => {
+        btn.classList.remove('active')
+    })
+}
+
+function updateIndexPage() {
+    checkConnectStatus();
+}
+function checkConnectStatus() {
+    let connexionParams = new URLSearchParams(document.location.search);
+    let connexionStatus = connexionParams.get('connected');
+
+    if(connexionStatus === '1') {
+        displayHiddenEls()
     }
 }
 
-let connexionParams = new URLSearchParams(document.location.search);
-let connexionStatus = connexionParams.get('connected');
+updateIndexPage()
 
-if(connexionStatus === '1') {
-    let updateBtns = document.querySelectorAll('.update-btn');
-    
-    for( let i=0; i<updateBtns.length; i++) {
-        updateBtns[i].classList.remove('hidden')
-    }
+function displayHiddenEls() {
+    document.querySelectorAll('.update-btn').forEach(btn => {
+        btn.classList.remove('hidden');
+        btn.addEventListener('click', () => {
+            let modal = document.querySelector('#modal');
+            let overlay = document.querySelector('.overlay');
+
+            modal.classList.remove('hidden');
+            overlay.classList.remove('hidden');
+
+            document.querySelector('.js-close-modal').addEventListener('click', () => { 
+                modal.classList.add('hidden');
+                overlay.classList.add('hidden'); 
+            });
+            overlay.addEventListener('click', () => {   
+                modal.classList.add('hidden');
+                overlay.classList.add('hidden'); 
+            }); 
+        })
+    })
 }
+
+function displayModalGallery() {
+    for( let i=0; i<works.length; i++) {
+        document.querySelector('.modal__gallery').innerHTML += `
+        <figure>
+            <a href="${works[i].imageUrl}">
+                <img src="${works[i].imageUrl}" alt="${works[i].title}"}>
+            </a>
+            <figcaption>
+                <span class="modal__icons">                
+                    <i class="fa-solid fa-arrows-up-down-left-right grow-up"></i>
+                    <i class="fa-regular fa-trash-can"></i>
+                </span>
+                <button>éditer</button>
+            </figcaption>
+        </figure>`
+    }           
+}
+displayModalGallery()
